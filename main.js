4431e46a24d66d57b0697972f13e0450ee132366
FrostedIce <FrostedIce2005@outlook.com>
2019-04-20 23:14:31 +0800

    Almost-Finished the design of the main window

--- main.js
+++ main.js
@@ -1,39 +1,40 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow, electron} = require('electron')
-
+const {app, BrowserWindow} = require('electron')
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
 
 function createWindow () {
-  // Create the browser window.
-  mainWindow = new BrowserWindow({
-    width: 600,
-    height: 500,
-    resizable: false,
-    title: 'Picker',
-    webPreferences: {
-      nodeIntegration: true
-    }
-    
-  })
-  mainWindow.setMenu(null)
+	// Create the browser window.
+	mainWindow = new BrowserWindow({
+		width: 600,
+		height: 500,
+		maxwidth: 600,
+		maxHeight: 500,
+		resizable: false,
+		frame: false,
+		title: 'Picker',
+		webPreferences: {
+		nodeIntegration: true
+		}
+		
+	})
+	mainWindow.setMenu(null)
 
 
-  // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+	// and load the index.html of the app.
+	mainWindow.loadFile('index.html')
 
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
-
-  // Emitted when the window is closed.
-  mainWindow.on('closed', function () {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null
-  })
+	// Open the DevTools.
+	// mainWindow.webContents.openDevTools()
+	// Emitted when the window is closed.
+	mainWindow.on('closed', function () {
+		// Dereference the window object, usually you would store windows
+		// in an array if your app supports multi windows, this is the time
+		// when you should delete the corresponding element.
+		mainWindow = null
+	})
 }
 
 // This method will be called when Electron has finished
@@ -43,16 +44,22 @@ app.on('ready', createWindow)
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') app.quit()
+	// On macOS it is common for applications and their menu bar
+	// to stay active until the user quits explicitly with Cmd + Q
+	if (process.platform !== 'darwin') app.quit()
 })
 
 app.on('activate', function () {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) createWindow()
+	// On macOS it's common to re-create a window in the app when the
+	// dock icon is clicked and there are no other windows open.
+	if (mainWindow === null) createWindow()
 })
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
+
+const ipc = require('electron').ipcMain;
+
+ipc.on('window-close',function(){
+	mainWindow.close();
+});
